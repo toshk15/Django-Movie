cfd2fb73ade9a57737a1d0b03195b5d629741d72
Fernando <toshk15@hotmail.com>
2025-08-28 16:17:11 -0600

    Model views Serializer

--- movie/movielist_app/api/serializers.py
+++ movie/movielist_app/api/serializers.py
@@ -1,25 +1,17 @@
 from rest_framework import serializers
 from movielist_app.models import Movie
 
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError("Name is too short!")
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        #fields = ['id','name','description']
+        #exclude = ['active']
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    def get_len_name(self, object):
+        return len(object.name)
 
     def validate(self, data):
         if data['name'] == data['description']:
