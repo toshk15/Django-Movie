cdb251ecbe348137bc52f727e4f564ef5c09839d
Fernando <toshk15@hotmail.com>
2025-08-28 21:15:02 -0600

    new model review added

--- movie/movielist_app/admin.py
+++ movie/movielist_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from movielist_app.models import Movie
+from movielist_app.models import WatchList, StreamPlatform, Review
 
-admin.site.register(Movie)
+admin.site.register(WatchList)
+admin.site.register(StreamPlatform)
+admin.site.register(Review)
 
-# Register your models here.
+
--- movie/movielist_app/api/serializers.py
+++ movie/movielist_app/api/serializers.py
@@ -1,26 +1,31 @@
 from rest_framework import serializers
-from movielist_app.models import Movie
+from movielist_app.models import WatchList, StreamPlatform, Review
 
-class MovieSerializer(serializers.ModelSerializer):
-    len_name = serializers.SerializerMethodField()
+class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
 
     class Meta:
-        model = Movie
+        model = Review      
         fields = "__all__"
-        #fields = ['id','name','description']
-        #exclude = ['active']
 
-    def get_len_name(self, object):
-        return len(object.name)
+class WatchListSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only= True)
+    class Meta:
+        model = WatchList
+        fields = "__all__"
 
-    def validate(self, data):
-        if data['name'] == data['description']:
-            raise serializers.ValidationError("Title and Description should be different!")
-        else:
-            return data
 
-    def validate_name(self, value):
-        if len(value) < 2:
-            raise serializers.ValidationError("Name is too short!")
-        else:
-            return value
+
+class StreamPlatformSerializer(serializers.ModelSerializer): 
+    
+    watchlist = WatchListSerializer(many=True, read_only= True)
+    #watchlist = serializers.StringRelatedField(many=True)
+    #serializers.HyperlinkedRelatedField(
+    #    many=True,
+    #    read_only=True,
+    #    view_name='movie-detail'
+    #)
+
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
